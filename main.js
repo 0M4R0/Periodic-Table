af46ce40d3e99299d7eef546019ec3d06f7a480c
Ronald <86748566+0M4R0@users.noreply.github.com>
2023-05-31 18:50:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+alert("Este es un intento de Tabla Periodica, completamente Html y CSS.")
+
+// Este codigo si sirve, pasa que se ve mal la celda
+
+
+// const celda = document.getElementById("celda-id");
+
+// celda.addEventListener("mouseover", function () {
+//     // Agrega aquí el código para animar la celda al hacer mouseover
+// });
+
+// celda.addEventListener("mouseout", function () {
+//     // Agrega aquí el código para revertir la animación al quitar el mouse
+// });
+
+// celda.addEventListener("mouseover", function () {
+//     celda.style.transform = "scale(1.2)"; // Hace la celda un 20% más grande
+//     // Agrega aquí el código para mostrar más información en la celda
+// });
+
+// celda.addEventListener("mouseout", function () {
+//     celda.style.transform = "scale(1)"; // Restablece el tamaño original de la celda
+//     // Agrega aquí el código para ocultar la información adicional de la celda
+// });
+
+// celda.addEventListener("mouseover", function () {
+//     const informacion = celda.querySelector(".informacion");
+//     informacion.classList.add("mostrar");
+// });
+
+// celda.addEventListener("mouseout", function () {
+//     const informacion = celda.querySelector(".informacion");
+//     informacion.classList.remove("mostrar");
+// });
+
